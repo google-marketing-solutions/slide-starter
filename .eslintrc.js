1580dd10e22c43583083372e8795ee28b1a8e5ef
Maria Zoe Lopez-Latorre <68270812+notmariazoe@users.noreply.github.com>
2022-11-16 18:49:57 +0000

    Update .eslintrc.js
    I honestly don't think this is the problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 {
-  "extends": ["eslint:recommended", "google"],
-  "parserOptions": {
-    "ecmaVersion": 8,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true,
+  extends: ["eslint:recommended", "google"],
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
     }
   },
-  "env": {
-    "node": true,
-    "es6": true,
-    "mocha": true,
+  env: {
+    node: true,
+    es6: true,
+    mocha: true,
   },
-  "rules": {
-    "prefer-promise-reject-errors": "off",
+  rules: {
+    prefer-promise-reject-errors: "off",
   }
 }
