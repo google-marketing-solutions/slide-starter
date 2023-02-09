662ec02fa164e10e117bb61df9cfe52fc3694517
Maria Zoe Lopez-Latorre <68270812+notmariazoe@users.noreply.github.com>
2023-02-09 08:53:11 +0000

    Added onOpen to apps starter

--- src/sls_apps.js
+++ src/sls_apps.js
@@ -1,4 +1,5 @@
 /* exported createDeckFromDatasource */
+/* exported onOpen */
 /**
   Copyright 2022 Google LLC
 
@@ -50,6 +51,26 @@ const COLUMN_WIDTH = [50, 50, 75, 250, 75, 180];
 const MAX_PAGE_HEIGHT = 175;
 const LINE_HEIGHT = 10;
 const ROW_HEIGHT = 20;
+
+/**
+ * A special function that runs when the spreadsheet is open, used to add a
+ * custom menu to the spreadsheet.
+ */
+function onOpen() {
+  loadConfiguration();
+  const spreadsheet = SpreadsheetApp.getActive();
+  const menuItems = [
+    {
+      name: 'Load configuration',
+      functionName: 'loadConfiguration',
+    },
+    {
+      name: 'Filter criteria and generate deck',
+      functionName: 'createDeckFromDatasource',
+    },
+  ];
+  spreadsheet.addMenu('MetricFlow Slide Starter', menuItems);
+}
 
 /**
  * Creates a presentation based on a set of recommendations included in a
