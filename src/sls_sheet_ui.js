f3c2c50dfdd4a74a73160d9b85372c828ab48b93
Nenu Adrian <adrian.nenu@gmail.com>
2023-04-13 15:36:26 +0100

    error messages

--- src/sls_sheet_ui.js
+++ src/sls_sheet_ui.js
@@ -6,7 +6,9 @@
 /* exported openUploadDialog */
 /* exported sheetUI */
 
-const ERROR_MISSING_VALUE= 'Please select a non-empty cell.';
+const ERROR_MISSING_VALUE = 'Please select a non-empty cell.';
+const ERROR_PARENT_FOLDER
+  = 'You do not have access to the parent folder of this Sheet.';
 const SUCCESS_UPLOADED = 'File uploaded for: ';
 
 /**
@@ -23,8 +25,17 @@ function sheetUI() {
  * Present HTML file Upload form.
  */
 function openUploadDialog() {
-  const html = HtmlService.createHtmlOutputFromFile('upload');
-  SpreadsheetApp.getUi().showModalDialog(html, 'Upload File to Images folder');
+  const activeCell = SpreadsheetApp.getActiveSheet().getActiveCell();
+  if (activeCell && activeCell.getValue() && activeCell.getValue().length > 0) {
+    const criteriaName = SpreadsheetApp.getActiveSheet().getRange(activeCell.getRow(), 1);
+    const html = HtmlService.createTemplateFromFile('upload');
+    html.criteriaName = criteriaName.getValue();
+    SpreadsheetApp.getUi().showModalDialog(html.evaluate(), 'Upload File to Images folder');
+  } else {
+    SpreadsheetApp.getActiveSpreadsheet()
+        .toast(ERROR_MISSING_VALUE);
+    throw new Error(ERROR_MISSING_VALUE);
+  }
 }
 
 /**
@@ -34,33 +45,35 @@ function openUploadDialog() {
  */
 function uploadFile(data, type) {
   const activeCell = SpreadsheetApp.getActiveSheet().getActiveCell();
-  if (activeCell && activeCell.getValue() && activeCell.getValue().length > 0) {
-    const fileName = activeCell.getValue();
+  const fileName = activeCell.getValue();
 
-    const thisFileId = SpreadsheetApp.getActive().getId();
-    const thisFile = DriveApp.getFileById(thisFileId);
+  const thisFileId = SpreadsheetApp.getActive().getId();
+  const thisFile = DriveApp.getFileById(thisFileId);
 
-    const parents = thisFile.getParents();
-    let f;
-    while (parents.hasNext()) {
-      f = parents.next();
-    }
-    const imagesFolders = f.getFoldersByName('Images');
-    let imageFolder;
-    if (!imagesFolders.hasNext()) {
-      imageFolder = f.createFolder('Images');
-    } else {
-      imageFolder = imagesFolders.next();
-    }
-    const imageBlob = Utilities.newBlob(
-        Utilities.base64Decode(data.split(',')[1]),
-        type,
-    );
-    imageBlob.setName(fileName);
-    imageFolder.createFile(imageBlob);
-    SpreadsheetApp.getActiveSpreadsheet()
-        .toast(SUCCESS_UPLOADED + criteriaId);
-  } else {
-    throw new Error(ERROR_MISSING_VALUE);
+  const parents = thisFile.getParents();
+  let f;
+  while (parents.hasNext()) {
+    f = parents.next();
   }
+
+  if (!f) {
+    SpreadsheetApp.getActiveSpreadsheet()
+        .toast(ERROR_PARENT_FOLDER);
+    throw new Error(ERROR_PARENT_FOLDER);
+  }
+  const imagesFolders = f.getFoldersByName('Images');
+  let imageFolder;
+  if (!imagesFolders.hasNext()) {
+    imageFolder = f.createFolder('Images');
+  } else {
+    imageFolder = imagesFolders.next();
+  }
+  const imageBlob = Utilities.newBlob(
+      Utilities.base64Decode(data.split(',')[1]),
+      type,
+  );
+  imageBlob.setName(fileName);
+  imageFolder.createFile(imageBlob);
+  SpreadsheetApp.getActiveSpreadsheet()
+      .toast(SUCCESS_UPLOADED + criteriaId);
 }
