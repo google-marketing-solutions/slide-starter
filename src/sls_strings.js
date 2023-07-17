ba058c9c0f2f6e0ad003e8bbb07d13f5ee0d3065
Maria Zoe Lopez-Latorre <68270812+notmariazoe@users.noreply.github.com>
2023-07-17 10:26:29 +0100

    Create sls_strings.js
    Separates all the common strings from their files. Needs JSDoc

---
+++ src/sls_strings.js
@@ -0,0 +1,10 @@
+// Error messages
+const ERROR_MISSING_RANGE = 'Couldn\'t find the named range in Configuration.';
+const ERROR_NO_SHAPE = 'There was a problem retrieving the shape layout.';
+
+// Properties configuration
+const RANGE_NAME = 'Configuration!PROPERTIES';
+
+// Warning messages
+const WARNING_NO_IMAGES = 'No image found for criteria id ';
+const WARNING_MULTIPLE_IMAGES = 'No image found for criteria id ';
