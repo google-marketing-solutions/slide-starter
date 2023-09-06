e64504da2fdc5e8c1365e3d790e01c69e399f607
theneekz <nicholascsward@gmail.com>
2023-09-06 12:03:32 -0500

    Updated UX for v4

--- src/sls_ux.js
+++ src/sls_ux.js
@@ -1,26 +1,46 @@
-/* exported createSlidesUX */
-/* exported parseFieldsAndCreateSlideSustainability */
+/* exported appendInsightDeck */
+/* exported applyCustomStyle */
+/* exported onOpen */
+
 /**
- * Google AppScript File
- * @fileoverview Script used in the UX Starter project to automate UX audits.
+ * @license
+ * Copyright 2023 Google LLC
  *
- * UX Starter V7 - 04/04/23
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-// Error messages
-// const ERROR_NO_SPREADSHEET = 'UX Starter must be attached to a spreadsheet.'
-// const ERROR_NO_DECK = 'There was a problem opening the generated deck.';
-// const ERROR_CREATING_IMAGES = 'There was a problem creating the image
-// mockups.'; const ERROR_MULTIPLE_FOLDERS = 'Please ensure there is only one
-// folder named ';
+/**
+ * Google AppScript File
+ * @fileoverview UX specific functions used to automate audit generation.
+ * - onOpen
+ *   Special function that runs when spreadsheet is open, used to load settings
+ *   and create the Katalyst menu.
+ *
+ * - appendInsightDeck
+ *   Finds and attaches a deck of insights for a given criteria.
+ *
+ * - retrieveImage
+ *   Finds an image from the "Images" parent folder.
+ *
+ * - applyCustomStyle
+ *   Unused function for UX but kept in order to not throw an error in core.gs
+ */
+
 
 // Warning messages
 const WARNING_NO_IMAGES = 'No image found for criteria id ';
 const WARNING_MULTIPLE_IMAGES = 'No image found for criteria id ';
 
-/* exported applyCustomStyle */
-/* exported onOpen */
-/* exported parseFieldsAndCreateSlide */
 
 /**
  * A special function that runs when the spreadsheet is open, used to add a
@@ -31,158 +51,79 @@ function onOpen() {
   const spreadsheet = SpreadsheetApp.getActive();
   const menuItems = [
     {
-      name: 'Load configuration',
-      functionName: 'loadConfiguration',
-    },
-    {
-      name: 'Filter criteria only',
-      functionName: 'filterAndSortData',
+      name: 'Upload Image',
+      functionName: 'uploadImageDialog',
     },
     {
       name: 'Generate deck',
-      functionName: 'createDeckFromDatasources',
+      functionName: 'createDeckFromDatasource',
     },
   ];
   spreadsheet.addMenu('Katalyst', menuItems);
-
-  sheetUI();
 }
 
+
 /**
- * Creates a collection of slides from data in a spreadsheet.
+ * Opens Presentation with the newDeckId and appends a deck of insight slides
+ * based on the first item of the row. Will add client & best practice images.
  *
- * @param {Deck} deck The deck to which the slides will be added.
- * @param {InsightDeck} insightDeck The insight deck to which the slides will be
- *     added.
- * @param {SlideLayout} slideLayout The slide layout to use for the slides.
+ * @param {!string} newDeckId Id of the new slide deck that has
+ * been generated.
+ * @param {array} row An array of text from a spreadsheet row containing
+ * [insightDeckName, unused, clientImageName, bestPracticeImageName] where
+ * insightDeckName is the title of the criteria & insight deck filename, unused
+ * is necessary in the sheet but not used in this function, and clientImageName
+ * & bestPracticeImageName are filenames to replace the client-mockup and
+ * best-practice shapes in the insight slides.
  */
-function createSlidesUX(deck, insightDeck, slideLayout) {
-  const spreadsheet = SpreadsheetApp.getActive().getSheetByName(
-      documentProperties.getProperty('DATA_SOURCE_SHEET'));
-  filterAndSortData();
-  const values = spreadsheet.getFilter().getRange().getValues();
-  for (let i = 1; i < values.length; i++) {
-    if (spreadsheet.isRowHiddenByFilter(i + 1)) {
-      continue;
+function appendInsightDeck(newDeckId, row) {
+  // Title of the criteria, column A, is the name of the deck to search for
+  // File name for the client image, column B, is clientImageName
+  // eslint-disable-next-line no-unused-vars
+  const [insightDeckName, unused, clientImageName, bestPracticeImageName] = row;
+  const currentDeck = SlidesApp.openById(newDeckId);
+  const insightFolderID = documentProperties.getProperty('INSIGHTS_FOLDER_ID');
+  const insightFolder = DriveApp.getFolderById(insightFolderID);
+  const fileIterator = insightFolder.getFilesByName(insightDeckName);
+  // There should only be one file in each folder for now
+  while (fileIterator.hasNext()) {
+    const insightDeckId = fileIterator.next().getId();
+    const insightDeck = SlidesApp.openById(insightDeckId).getSlides();
+    for (const slide of insightDeck) {
+      const newSlide = currentDeck.appendSlide(slide, SlidesApp.SlideLinkingMode.NOT_LINKED);
+      try {
+        const folder = getImagesFolder();
+        const clientImageSrc = retrieveImage(folder, clientImageName);
+        const clientShape = retrieveShape(newSlide, 'client-mockup', false);
+        const bestPracticeImageSrc = retrieveImage(folder, bestPracticeImageName);
+        const bestPracticeShape = retrieveShape(newSlide, 'best-practice', false);
+
+        // Insert images and move behind checkmarks & phone border
+        const insertedClientImage = newSlide.insertImage(
+            clientImageSrc, clientShape.getLeft(), clientShape.getTop(),
+            clientShape.getWidth(), clientShape.getHeight());
+        insertedClientImage.sendToBack();
+        for (let i = 0; i < 3; i++) {
+          insertedClientImage.bringForward();
+        }
+        const insertedBestPracticeImage = newSlide.insertImage(
+            bestPracticeImageSrc, bestPracticeShape.getLeft(), bestPracticeShape.getTop(),
+            bestPracticeShape.getWidth(), bestPracticeShape.getHeight());
+        insertedBestPracticeImage.sendToBack();
+        for (let i = 0; i < 4; i++) {
+          insertedBestPracticeImage.bringForward();
+        }
+      } catch (e) {
+        console.log('error uploading an image:', e);
+      }
     }
-    const row = values[i];
-    parseFieldsAndCreateSlide(deck, insightDeck, slideLayout, row);
   }
 }
 
-
-/**
- * Parses the fields contained on the incoming row from the spreadsheet into
- * some specific information fields, and then creates the slide using GAS.
- *
- * @param {string} deck Id of the generated deck that will contain the recos
- * @param {!Presentation} insightDeck Reference to the generated deck
- * @param {!Layout} recommendationSlideLayout The template layout
- * @param {!Array<string>} row Array of strings with information from the
- *     spreadsheet
- */
-function parseFieldsAndCreateSlide(
-    deck, insightDeck, recommendationSlideLayout, row) {
-  const criteriaIdIndex =
-      documentProperties.getProperty('UX_CRITERIA_ID_COLUMN') - 1;
-  const criteriaNameIndex = documentProperties.getProperty('TITLE_COLUMN') - 1;
-  const criteriaAppliesIndex =
-      documentProperties.getProperty('SUBTITLE_COLUMN') - 1;
-  const criteriaProblemStatementIndex =
-      documentProperties.getProperty(
-          'UX_RECOMMENDATIONS_PROBLEM_STATEMENT_ROW') -
-      1;
-  const criteriaSolutionStatementIndex =
-      documentProperties.getProperty(
-          'UX_RECOMMENDATIONS_SOLUTION_STATEMENT_ROW') -
-      1;
-  const criteriaImageMockupIndex =
-      documentProperties.getProperty('UX_IMAGE_MOCKUP_COLUMN') - 1;
-  const criteriaDefaultImageUrl =
-      documentProperties.getProperty('UX_DEFAULT_IMAGE_MOCKUP');
-  const criteriaInsightSlidesIndex =
-      documentProperties.getProperty('INSIGHT_SLIDE_ID_COLUMN') - 1;
-
-  const criteriaId = row[criteriaIdIndex];
-  const criteria = row[criteriaNameIndex];
-  const applicable =
-      `Applies for: ${row[criteriaAppliesIndex].split(',').join(',')}`;
-  const description = row[criteriaProblemStatementIndex] + '\n' +
-      row[criteriaSolutionStatementIndex];
-  const imageMockup =
-      (row[criteriaImageMockupIndex] === '' ? criteriaDefaultImageUrl :
-                                              row[criteriaImageMockupIndex]);
-  const insights = row[criteriaInsightSlidesIndex].split(',');
-  let folder = DriveApp.getFileById(SpreadsheetApp.getActive().getId())
-      .getParents()
-      .next();
-  // use Images folder instead of root if exists
-  const imagesFolders = folder.getFoldersByName('Images');
-  if (!imagesFolders.hasNext()) {
-    folder = imagesFolders.next();
-  }
-  const clientImage = retrieveClientImage(folder, criteriaId);
-  createRecommendationSlideGAS(
-      deck, recommendationSlideLayout, criteria, applicable, description,
-      imageMockup, clientImage);
-  if (insights.length > 0) {
-    appendInsightSlides(deck, insightDeck, insights);
-  }
-}
-
-/**
- * Creates the slides programmatically using the SlidesApp from AppScript:
- * It first creates a new slide with the specified layout, it populates the
- * placeholders with the corresponding values passed as parameter.
- * In case there are insight slide ids in the insights array, it will
- * instantiate those slides and append them to the created slide
- *
- * @param {!Presentation} deck Id of the generated deck that will contain the
- *     recos
- * @param {!Layout} recommendationSlideLayout The template layout
- * @param {string} criteria The name of the criteria used as title
- * @param {string} applicable A list of pages where this criteria is applicable
- * @param {string} description The description of the failing criteria
- * @param {string} imageMockup The URL of the image corresponding to the mockup
- * @param {string} clientImage The image file of the client screenshot
- */
-function createRecommendationSlideGAS(
-    deck, recommendationSlideLayout, criteria, applicable, description,
-    imageMockup, clientImage) {
-  const slide = deck.appendSlide(recommendationSlideLayout);
-  if (deck.getMasters().length > 1) {
-    deck.getMasters()[deck.getMasters().length - 1].remove();
-  }
-
-  const titlePlaceholder =
-      slide.getPlaceholder(SlidesApp.PlaceholderType.TITLE);
-  const subtitlePlaceholder =
-      slide.getPlaceholder(SlidesApp.PlaceholderType.SUBTITLE);
-  const bodyPlaceholder = slide.getPlaceholder(SlidesApp.PlaceholderType.BODY);
-
-  const titleRange = titlePlaceholder.asShape().getText();
-  titleRange.setText(criteria);
-
-  const subtitleRange = subtitlePlaceholder.asShape().getText();
-  subtitleRange.setText(applicable);
-
-  const bodyRange = bodyPlaceholder.asShape().getText();
-  bodyRange.setText(description);
-
-  const baseShape = retrieveShape(slide, 'best-practice');
-  const clientShape = retrieveShape(slide, 'client-mockup');
-
-  slide.insertImage(
-      imageMockup, baseShape.getLeft(), baseShape.getTop(),
-      baseShape.getWidth(), baseShape.getHeight());
-  slide.insertImage(
-      clientImage, clientShape.getLeft(), clientShape.getTop(),
-      clientShape.getWidth(), clientShape.getHeight());
-}
 
 /**
  * Returns a file, which is assumed to be an image file, for a criteria client
- * screenshot which should be named after a criteria id. Any formats are
+ * screenshot which should be named after a criteria. Any formats are
  * considered for the query, but it is assumed that the file will be an image.
  *
  * This file is retrieved from a folder created programmatically which is
@@ -190,16 +131,15 @@ function createRecommendationSlideGAS(
  *
  * If no such file has been found, the function returns the url of the default
  * image mockup, which behaves analogously to an image file.
- *
  * There are warnings sent out (currently as a toast on the spreadsheet) in this
  * case, and also in case that multiple image files are found. When finding
  * multiple images, the last one is selected.
  *
  * @param {!Folder} folder The folder where images are being stored.
- * @param {string} criteriaId String corresponding to the criteria id.
+ * @param {string} criteriaId String corresponding to the image name.
  * @return {?*} Image file for the screenshot or a string url for the default
  */
-function retrieveClientImage(folder, criteriaId) {
+function retrieveImage(folder, criteriaId) {
   const searchQuery = `title contains '${criteriaId}'
       and mimeType contains 'image'`;
   const files = folder.searchFiles(searchQuery);
@@ -217,119 +157,19 @@ function retrieveClientImage(folder, criteriaId) {
   }
 
   if (file === null) {
-    file = PropertiesService.getDocumentProperties().getProperty(
-        'UX_DEFAULT_IMAGE_MOCKUP');
+    file = documentProperties.getProperty('UX_DEFAULT_IMAGE_MOCKUP');
   }
 
   return file;
 }
 
 /**
- * Applies any extra operations to the deck based on the specifics of the audit
+ * Applies any extra operations to the deck based on the specifics of the audit.
  *
- * @param {string} newDeckId Id of the generated deck that will contain the
- *     recos
+ * @param {!string} newDeckId Id of the new slide deck that has
+ * been generated.
  */
 function applyCustomStyle(newDeckId) {
-  const deck = SlidesApp.openById(newDeckId);
-  const insightDeck =
-      SlidesApp.openById(documentProperties.getProperty('INSIGHTS_DECK_ID'));
-  const endSlideId = documentProperties.getProperty('END_SLIDE_ID');
-  const endSlide = insightDeck.getSlideById(endSlideId.trim());
-  deck.appendSlide(endSlide, SlidesApp.SlideLinkingMode.NOT_LINKED);
+  return;
 }
 
-// ----- Sustainability benchmark
-
-/**
- * Parses the fields and creates a sustainability slide. This function prepares
- * the data from the recommendations sheet and builds a chart that is inserted
- * into the slide. The chart is taken from a sheet specified in the constants.
- * The slide is appended to the presentation specified by the deck parameter.
- *
- * @param {GoogleAppsScript.Slides.Presentation} deck The slide deck to which
- *     the slide should be appended.
- * @param {GoogleAppsScript.Slides.Presentation} insightDeck The slide deck that
- *     contains the insights slides to be used as context.
- * @param {GoogleAppsScript.Slides.PageElement} slideLayout The layout of the
- *     slide to be created.
- */
-function parseFieldsAndCreateSlideSustainability(
-    deck, insightDeck, slideLayout) {
-  const presentationId = deck.getId();
-  // Preparing the data and adding it into the chart
-  const spreadsheet = SpreadsheetApp.getActive().getSheetByName(
-      documentProperties.getProperty('RECOMMENDATIONS_SHEET'));
-  filterAndSortData(spreadsheet);
-  const values = spreadsheet.getFilter().getRange().getValues();
-  const chartSheetName = documentProperties.getProperty('DATA_SOURCE_SHEET');
-  buildReadinessAnalysis(spreadsheet, values, chartSheetName);
-
-  // Retrieving and inserting the chart
-  const chartSheet = SpreadsheetApp.getActive().getSheetByName(chartSheetName);
-  const spreadsheetId = SpreadsheetApp.getActive().getId();
-  const sheetChartId = chartSheet.getCharts()[0].getChartId();
-  const slide = deck.appendSlide(slideLayout);
-  if (deck.getMasters().length > 1) {
-    deck.getMasters()[deck.getMasters().length - 1].remove();
-  }
-  const slidePageId = slide.getObjectId();
-  const slideShape = retrieveShape(slide, 'chart-location');
-  deck.saveAndClose();
-  replaceSlideShapeWithSheetsChart(
-      presentationId, spreadsheetId, sheetChartId, slidePageId, slideShape);
-}
-
-/**
- * Builds the readiness analysis chart based on the recommendations data. This
- * function is called by parseFieldsAndCreateSlideSustainability and is
- * responsible for building the chart that is inserted into the slide. It reads
- * the configuration data from the document properties and uses the
- * recommendations sheet to calculate the values to be displayed in the chart.
- *
- * @param {GoogleAppsScript.Spreadsheet.Sheet} spreadsheet The sheet containing
- *     the recommendations data.
- * @param {Array} values The array of values returned by the filter on the
- *     sheet.
- * @param {string} chartSheetName The name of the sheet that contains the chart
- *     to be used.
- */
-function buildReadinessAnalysis(spreadsheet, values, chartSheetName) {
-  const documentProperties = PropertiesService.getDocumentProperties();
-  const policyNamesListString =
-      documentProperties.getProperty('CATEGORY_NAMES_LIST');
-  const policyNamesList =
-      policyNamesListString.split(',').map((item) => item.trim());
-  const policyColumnIndex =
-      documentProperties.getProperty('POLICY_MAPPING_COLUMN') - 1;
-  const policyValuesList = new Array(policyNamesList.length).fill(0);
-  const policyTotalList = new Array(policyNamesList.length).fill(0);
-
-  for (const row of values) {
-    if (!row[policyColumnIndex]) {
-      continue;
-    }
-    const rowPolicyArray =
-        row[policyColumnIndex].split(',').map((item) => item.trim());
-    for (const policyName of rowPolicyArray) {
-      const policyZeroIndex = policyNamesList.indexOf(policyName);
-      policyTotalList[policyZeroIndex]++;
-      const rowZeroIndex = values.indexOf(row);
-      if (spreadsheet.isRowHiddenByFilter(rowZeroIndex + 1)) {
-        continue;
-      }
-      policyValuesList[policyZeroIndex]++;
-    }
-  }
-
-  const partialValuesRange = '\'' + chartSheetName + '\'!' +
-      documentProperties.getProperty('PARTIAL_RESULTS_RANGE');
-  const totalValuesRange = '\'' + chartSheetName + '\'!' +
-      documentProperties.getProperty('TOTAL_RESULTS_RANGE');
-  SpreadsheetApp.getActive().getRangeByName(partialValuesRange).setValues([
-    policyValuesList,
-  ]);
-  SpreadsheetApp.getActive().getRangeByName(totalValuesRange).setValues([
-    policyTotalList,
-  ]);
-}
